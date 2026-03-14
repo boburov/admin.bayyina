2e9969164be03a864105c671f966f3891667e751
yaxyobekuz <pubgn9642@gmail.com>
2026-03-14 10:57:55 +0500

    feat: add BackgroundPatterns component

--- src/shared/layouts/DashboardLayout.jsx
+++ src/shared/layouts/DashboardLayout.jsx
@@ -52,7 +52,7 @@ const DashboardLayout = () => {
   return (
     <>
       {/* Main */}
-      <SidebarProvider>
+      <SidebarProvider className="relative z-10">
         <AppSidebar />
         <SidebarInset>
           <AppHeader />
@@ -61,6 +61,9 @@ const DashboardLayout = () => {
           </div>
         </SidebarInset>
       </SidebarProvider>
+
+      {/* Background Patterns */}
+      <BackgroundPatterns />
 
       {/* User Modals */}
       <EditUserModal />
@@ -183,5 +186,21 @@ const actions = () => {
     if (!hasEntity("today")) checkTodayHoliday();
   }, [classes?.length, subjects?.length, teachers?.length]);
 };
+
+const BackgroundPatterns = () => (
+  <>
+    {/* Grid */}
+    <div className="fixed inset-0 z-0 h-full w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:24px_24px]" />
+
+    {/* Blue Circle */}
+    <div className="fixed left-0 right-0 top-[-10%] z-0 m-auto h-[300px] w-[300px] rounded-full bg-blue-500 opacity-20 blur-[100px]" />
+
+    {/* Indigo Circle */}
+    <div className="fixed bottom-[-10%] left-[-10%] z-0 h-[300px] w-[300px] rounded-full bg-indigo-500 opacity-20 blur-[100px]" />
+
+    {/* Purple Circle */}
+    <div className="fixed top-[20%] right-[-5%] z-0 h-[250px] w-[250px] rounded-full bg-purple-500 opacity-20 blur-[100px]" />
+  </>
+);
 
 export default DashboardLayout;
