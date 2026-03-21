4d626ee14ac08f61bbd700e3e39898ef030dcfd1
yaxyobekuz <pubgn9642@gmail.com>
2026-03-21 17:50:57 +0500

    feat: enhance Button component to allow optional click sound playback

--- src/shared/components/ui/button/Button.jsx
+++ src/shared/components/ui/button/Button.jsx
@@ -4,16 +4,16 @@ import useSound from "@/shared/hooks/useSound";
 // Components
 import { Button as ButtonComponent } from "@/shared/components/shadcn/button";
 
-const Button = ({ children, ...props }) => {
+const Button = ({ children, playClickSound = true, ...props }) => {
   const { playSound } = useSound();
+
+  const handleClick = (e) => {
+    props.onClick?.(e);
+    playClickSound && playSound("click-button");
+  };
+
   return (
-    <ButtonComponent
-      {...props}
-      onClick={(e) => {
-        props?.onClick(e);
-        playSound("click-button");
-      }}
-    >
+    <ButtonComponent {...props} onClick={handleClick}>
       {children}
     </ButtonComponent>
   );
