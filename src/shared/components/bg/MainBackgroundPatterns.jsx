cceba90703b6d551697cef848da01e9927e7187b
yaxyobekuz <pubgn9642@gmail.com>
2026-04-04 17:58:44 +0500

    feat: update color scheme to use blue instead of indigo across various components

--- src/shared/components/bg/MainBackgroundPatterns.jsx
+++ src/shared/components/bg/MainBackgroundPatterns.jsx
@@ -6,8 +6,8 @@ const MainBackgroundPatterns = () => (
     {/* Blue Circle */}
     <div className="fixed left-0 right-0 top-[-10%] z-0 m-auto h-[300px] w-[300px] rounded-full bg-blue-500 opacity-20 blur-[100px]" />
 
-    {/* Indigo Circle */}
-    <div className="fixed bottom-[-10%] left-[-10%] z-0 h-[300px] w-[300px] rounded-full bg-indigo-500 opacity-20 blur-[100px]" />
+    {/* blue Circle */}
+    <div className="fixed bottom-[-10%] left-[-10%] z-0 h-[300px] w-[300px] rounded-full bg-blue-500 opacity-20 blur-[100px]" />
 
     {/* Purple Circle */}
     <div className="fixed top-[20%] right-[-5%] z-0 h-[250px] w-[250px] rounded-full bg-purple-500 opacity-20 blur-[100px]" />
